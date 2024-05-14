4b944ef007e058c3ff7d9a48aad643640046f637
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-05-14 22:43:29 +0530

    Changes

--- index.js
+++ index.js
@@ -3,9 +3,31 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connection = require("./db");
+const mongoose = require("mongoose");
+const { MongoClient } = require('mongodb');
 
-//database connection
-connection();
+//database connection Local DB
+// mongoose
+//   .connect(process.env.DB)
+//   .then(console.log("Mongo DB Connected !"))
+//   .catch((error) => console.log(error));
+
+
+  //Databse connect atlas DB
+
+const client = new MongoClient(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToMongoDB() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB Atlas");
+    } catch (error) {
+        console.error("Error connecting to MongoDB Atlas", error);
+    }
+}
+
+connectToMongoDB();
+
 
 //middelwares
 
