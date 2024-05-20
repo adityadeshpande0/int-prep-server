c32f43ab1feddd76530471ba9e1b9565a99509be
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-05-21 00:14:08 +0530

    db config updated

--- db.js
+++ db.js
@@ -1,19 +1,15 @@
 // config/db.js
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.AtlasDB, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            
-        });
-        console.log('MongoDB connected');
-    } catch (err) {
-        console.error('MongoDB connection error:', err.message);
-        process.exit(1); // Exit process with failure
-    }
+  try {
+    await mongoose.connect(process.env.AtlasDB);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1); // Exit process with failure
+  }
 };
 
 module.exports = connectDB;
