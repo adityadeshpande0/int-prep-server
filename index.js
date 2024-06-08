72197d1a9e61c5f8517c2691c8319b84577f79ed
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-06-08 15:39:29 +0530

    Cors Multiple Origins

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connection = require("./db");
@@ -9,13 +8,23 @@ const app = express();
 // Database connection
 connection();
 
+// Define allowed origins
+const allowedOrigins = ['http://localhost:5173', 'https://intprep.netlify.app/'];
+
 // CORS Options
 const corsOptions = {
-  origin: 'http://localhost:5173', // Allow this origin to access the resource
+  origin: function (origin, callback) {
+    // Check if the request origin is in the allowedOrigins array
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); // Allow the request
+    } else {
+      callback(new Error('Not allowed by CORS')); // Block the request
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allow cookies to be sent/received in CORS requests
-  optionsSuccessStatus:200 , // Some legacy browsers choke on 204
+  optionsSuccessStatus: 200, // Some legacy browsers choke on 204
 };
 
 // Apply CORS middleware
