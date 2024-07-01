5f2611fea6b08d71de98c5a43e580370f95750a0
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-07-02 00:04:19 +0530

    response messages edited

--- controllers/scheduleInterviewController.js
+++ controllers/scheduleInterviewController.js
@@ -1,25 +1,37 @@
-const InterviewRequest = require('../models/bookings');
+const InterviewRequest = require("../models/bookings");
 
 exports.scheduleInterviewController = async (req, res) => {
-    try {
-        const { date, timeslot } = req.body; // Ensure the naming matches the schema
-        const userId = req.user ? req.user.id : null; // Adjust to req.user.id
+  try {
+    const { date, timeslot } = req.body;
+    const userId = req.user ? req.user.id : null;
 
-        // Debugging output
-        console.log('Request body:', req.body);
-        console.log('Decoded user:', req.user);
-        console.log('User ID:', userId);
+    // Debugging output
+    console.log("Request body:", req.body);
+    console.log("Decoded user:", req.user);
+    console.log("User ID:", userId);
 
-        const newInterviewRequest = new InterviewRequest({
-            userId,
-            date,
-            timeslot // Ensure the naming matches the schema
-        });
+    // Check if the time slot on the specified date is already booked by any user
+    const existingInterviewRequest = await InterviewRequest.findOne({
+      date,
+      timeslot,
+    });
 
-        await newInterviewRequest.save();
-        res.status(201).json(newInterviewRequest);
-    } catch (error) {
-        console.error('Error scheduling interview:', error);
-        res.status(400).json({ error: 'Something went wrong!' });
+    if (existingInterviewRequest) {
+      return res
+        .status(400)
+        .json({ error: "Slot is already booked" });
     }
+
+    const newInterviewRequest = new InterviewRequest({
+      userId,
+      date,
+      timeslot,
+    });
+
+    await newInterviewRequest.save();
+    res.status(201).json({ message: "Request sent successfully !" });
+  } catch (error) {
+    console.error("Error scheduling interview:", error);
+    res.status(400).json({ error: "Something went wrong!" });
+  }
 };
